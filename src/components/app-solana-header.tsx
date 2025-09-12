2cb4e7d6bb5ce74ec9b7c9d8ecee76b78fda5a59
cxalem <smarketing.cuz@gmail.com>
2025-09-12 16:46:01 +0200

    style: enhance Solana header with improved layout and link styling

--- src/components/app-solana-header.tsx
+++ src/components/app-solana-header.tsx
@@ -8,16 +8,21 @@ export interface AppSolanaHeaderLink {
 
 export function AppSolanaHeader({ links }: { links: AppSolanaHeaderLink[] }) {
   return (
-    <header className="bg-black p-6">
+    <header className="w-full max-w-3xl rounded-full px-6 py-3 mx-auto mt-3 bg-neutral-900 border border-neutral-700">
       <div className="max-w-[1200] container mx-auto py-md px-6">
         <div className="flex justify-between">
           <a href="https://solana.com" rel="noopener noreferrer">
             <Image src={SolanaLogo} alt="Solana Logo" width={149} height={22} className="text-white" />
           </a>
 
-          <div className="flex gap-2 text-neutral-500">
+          <div className="flex gap-2">
             {links.map((link) => (
-              <a href={link.href} key={link.label} rel="noopener noreferrer">
+              <a
+                href={link.href}
+                key={link.label}
+                className="text-neutral-500 transition duration-200 ease-in-out hover:text-neutral-400"
+                rel="noopener noreferrer"
+              >
                 {link.label}
               </a>
             ))}
