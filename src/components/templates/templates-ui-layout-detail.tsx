1ab42e04bfc228907c19d2cf11c62e6e27e51860
cxalem <smarketing.cuz@gmail.com>
2025-09-13 18:52:28 +0200

    refactor: restructure TemplatesUiLayoutDetail for improved layout and responsiveness

--- src/components/templates/templates-ui-layout-detail.tsx
+++ src/components/templates/templates-ui-layout-detail.tsx
@@ -22,33 +22,33 @@ export function TemplatesUiLayoutDetail({ name, source }: { name: string; source
   }
 
   return (
-    <div className="grid md:grid-cols-4 gap-4 lg:gap-8">
-      <div className="md:col-span-1">
-        <div className="hidden md:block md:sticky md:top-4 md:self-start">
-          <TemplatesUiSidebarDetail template={template} />
-        </div>
-        <div className="md:hidden">
-          <Accordion type="single" collapsible className="w-full">
-            <AccordionItem value="item-1">
-              <AccordionTrigger>Template details</AccordionTrigger>
-              <AccordionContent>
-                <TemplatesUiSidebarDetail template={template} />
-              </AccordionContent>
-            </AccordionItem>
-          </Accordion>
-        </div>
-      </div>
+    <div>
       <div className="md:col-span-3 gap-4">
-        <div className="">
+        <div>
           <div>
-            <div className="border rounded-lg overflow-hidden mb-4">
-              <div className="w-full h-full flex items-center justify-center">
+            <div className="border rounded-lg overflow-hidden mb-4 max-w-5xl mx-auto">
+              <div className="w-full max-h-96 flex items-center justify-center">
                 <TemplatesUiImage template={template} />
               </div>
             </div>
           </div>
-          <div>
-            <div className="prose dark:prose-invert max-w-none border rounded-lg p-4 bg-neutral-50 dark:bg-neutral-900">
+          <div className="grid grid-cols-2 md:grid-cols-5 gap-4 lg:gap-8 max-w-5xl mx-auto">
+            <div className="col-span-2">
+              <div className="hidden md:block md:sticky md:top-4 md:self-start">
+                <TemplatesUiSidebarDetail template={template} />
+              </div>
+              <div className="md:hidden ">
+                <Accordion type="single" collapsible className="w-full">
+                  <AccordionItem value="item-1">
+                    <AccordionTrigger>Template details</AccordionTrigger>
+                    <AccordionContent>
+                      <TemplatesUiSidebarDetail template={template} />
+                    </AccordionContent>
+                  </AccordionItem>
+                </Accordion>
+              </div>
+            </div>
+            <div className="col-span-2 md:col-span-3 prose dark:prose-invert max-w-none border rounded-lg p-4 bg-neutral-50 dark:bg-violet-950/5 backdrop-blur-md">
               <div dangerouslySetInnerHTML={{ __html: template.readme }} />
             </div>
           </div>
